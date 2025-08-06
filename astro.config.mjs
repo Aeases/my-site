1a3f14913327a1e3a7c6badeb5c471ac8ff05dfb
Aeases <96164686+Aeases@users.noreply.github.com>
2025-08-06 20:30:40 +0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import sitemap from "@astrojs/sitemap"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import mkcert from "vite-plugin-mkcert";
-import vercel from "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel"
 import remarkBreaks from "remark-breaks"
 import remarkObsidianCallout from "remark-obsidian-callout";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://zanefitzgerald.com",
   markdown: {remarkPlugins: [remarkBreaks, remarkObsidianCallout]},
   integrations: [
     mdx(),
